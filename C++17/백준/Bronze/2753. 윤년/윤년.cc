4bc9b6da0a225aa982f8e85ec38258d86ef5517c
GirimNam <rlfla0721@naver.com>
2026-01-20 16:25:33 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2753. 윤년/윤년.cc
+++ C++17/백준/Bronze/2753. 윤년/윤년.cc
@@ -1,25 +1,17 @@
 #include <iostream>
- 
 using namespace std;
- 
-int main(int argc, char const *argv[]) {
-    int y;
- 
-    cin >> y;
- 
-    if (y % 4 == 0) {
-        if (y % 400 == 0) {     // 4의 배수이면서 400의 배수일 때(=윤년)
-            cout << 1;
-        } 
-        else if (y % 100 != 0) {    // 4의 배수이면서 100의 배수가 아닐 때(=윤년) 
-            cout << 1;
-        } 
-        else {
-            cout << 0;
-        }
-    } else {
-        cout << 0;
+
+int main(){
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    if ((N%4==0 && N%400==0)||(N%4==0 && N%100!=0))
+    {
+        cout << 1;
     }
- 
-    return 0;
+    else  cout << 0;
+    
 }
